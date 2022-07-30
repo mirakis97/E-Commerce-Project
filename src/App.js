19b1b453ecc898140048b82dfe1df6bc21fa136a
Miroslav Vasilev <vasilevm4@gmail.com>
2022-07-30 15:19:00 +0300

    Create list of product that i will use lately

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import data from "./componets/Data/data";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <a href="/">Shopyfly</a>
+        <a href="/">Shopyfly</a>
       </header>
       <main>
-        Some list
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="thumbs">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p><strong>${product.price}</strong></p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
