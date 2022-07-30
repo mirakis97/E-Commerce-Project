bc6a1740be95c033100a3d041ba25dfe46a7a7a0
Miroslav Vasilev <vasilevm4@gmail.com>
2022-07-30 15:56:05 +0300

    Added HomeScreen Component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
-import data from "./componets/Data/data";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeScreen from "./componets/Home/HomeScreen";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a href="/">Shopyfly</a>
-      </header>
-      <main>
-        <h1>Featured Products</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="thumbs">
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p><strong>${product.price}</strong></p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <a href="/">Shopyfly</a>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen />}/>
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
