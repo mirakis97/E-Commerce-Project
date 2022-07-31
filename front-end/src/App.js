cf157db12e0b0f23e331d631b3ba5976a01d7b7b
Miroslav Vasilev <vasilevm4@gmail.com>
2022-07-31 14:18:54 +0300

    Added Product Compoment

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,6 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeScreen from "./componets/Home/HomeScreen";
-import Product from "./componets/Products/Product";
+import ProdcutSlug from "./componets/Products/ProductSlug";
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import {LinkContainer} from 'react-router-bootstrap'
@@ -21,7 +21,7 @@ function App() {
         <main>
         <Container>
           <Routes>
-            <Route path="/product/:slug" element={<Product />} />
+            <Route path="/product/:slug" element={<ProdcutSlug />} />
             <Route path="/" element={<HomeScreen />} />
           </Routes>
           </Container>
--- front-end/src/componets/Products/Product.js
+++ front-end/src/componets/Products/Product.js
@@ -1,12 +1,22 @@
-import { useParams } from "react-router-dom";
-
-function Product() {
-  const params = useParams();
-  const { slug } = params;
+import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
+function Product(props) {
+  const { product } = props;
   return (
-    <div>
-      <h1>{slug}</h1>
-    </div>
+    <Card>
+      <Link to={`/product/${product.slug}`}>
+        <img src={product.image} className="card-img-top" alt={product.name} />
+      </Link>
+      <Card.Body>
+        <Link style={{textDecoration: 'none'}} to={`/product/${product.slug}`}>
+          <Card.Title>{product.name}</Card.Title>
+        </Link>
+        <Rating rating={product.rating} numReviews={product.numReviews}/>
+        <Card.Text><strong>${product.price}</strong></Card.Text>
+        <Button>Add to cart</Button>
+      </Card.Body>
+    </Card>
   );
 }
 export default Product;
